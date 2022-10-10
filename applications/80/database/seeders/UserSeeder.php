e3692fc15d7fc9902bdd1af885efc88653ab9e88
Thiago Philipe Budal <budal.thiago@gmail.com>
2022-10-10 13:36:10 -0300

    2022-10-10 13:35

---
+++ applications/80/database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Seeder;
+use App\Models\Team;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'SUPERADMIN';
+        $user->email = 'admin@localhost';
+        $user->password = Hash::make('password');
+        $user->email_verified_at = Carbon::now();
+        $user->save();
+
+        $superAdmin = User::where('email', 'admin@localhost')->first();
+
+        $user->ownedTeams()->save(Team::forceCreate([
+            'user_id' => $superAdmin->id,
+            'name' => $superAdmin->name,
+            'personal_team' => true,
+        ]));
+    }
+}
